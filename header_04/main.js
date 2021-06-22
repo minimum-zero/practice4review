3d997f22aa101477085484894c95f2b0653d05dc
minimum-zero <kolkori1115@gmail.com>
2021-06-22 22:49:13 +0900

    서브메뉴 개별오픈

--- header_04/main.js
+++ header_04/main.js
@@ -1,12 +1,15 @@
 const toggleBtn = document.querySelector('.toggle');
 const menuList = document.querySelector('.mainnav');
-const openSub = document.querySelector('.mainnav h3');
-const subMenu = document.querySelector('.subnav');
+const openSub = document.getElementsByTagName('h3');
+const subMenu = document.getElementsByClassName('subnav');
+
 
 toggleBtn.addEventListener('click', function(){
   menuList.classList.toggle('active');
 });
 
-openSub.addEventListener('click', function(){
-  subMenu.classList.toggle('active');
-});
+for(let i = 0; i < openSub.length; i++){
+	openSub[i].addEventListener('click', function(){
+		subMenu[i].classList.toggle('active');
+  });
+};
